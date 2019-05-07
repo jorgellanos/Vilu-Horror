bc96146287124ec08714ddbdb6ca4005895522cd
jorgellanos <jorg.llanos@hotmail.com>
2019-05-07 17:54:49 -0400

    reset event ADDED

---
+++ TestEnvironment/Assets/_Scripts/EventReset.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventReset : MonoBehaviour
+{
+    public bool touching;
+
+    public LightChanger lc;
+    public GameObject player;
+
+    public Vector3 posInicio;
+
+    private Hand currentHand;
+
+    void Start()
+    {
+        posInicio = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touching)
+        {
+            ResetLvl();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hand")
+        {
+            touching = true;
+            currentHand = other.GetComponent<Hand>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hand")
+        {
+            touching = false;
+            currentHand = null;
+        }
+    }
+
+    public void ResetLvl()
+    {
+        if (currentHand.pressing)
+        {
+            lc.num = 1;
+            player.transform.position = posInicio;
+        }
+    }
+}
--- TestEnvironment/Assets/_Scripts/LightChanger.cs
+++ TestEnvironment/Assets/_Scripts/LightChanger.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class LightChanger : MonoBehaviour
 {
     public float num;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public bool itsTime;
+    
     // Update is called once per frame
     void Update()
     {
         SetBlend(num);
+
+        if (itsTime)
+        {
+            cambiarSkybox();
+        }
     }
     
     public void SetBlend(float num)
     {
         RenderSettings.skybox.SetFloat("_Blend", num);
     }
-
-
+    
+    public void cambiarSkybox()
+    {
+        num = num - 0.1f * Time.deltaTime;
+        if (num <= 0)
+        {
+            itsTime = false;
+        }
+    }
 }
