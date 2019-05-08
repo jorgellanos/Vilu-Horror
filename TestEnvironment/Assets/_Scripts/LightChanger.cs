866b28212f3a6768fdfd744063576c4d87949d95
Puppiesonac1d <the_sorr0w@hotmail.com>
2019-05-08 15:23:56 -0400

    Revert "reset event ADDED"
    This reverts commit bc96146287124ec08714ddbdb6ca4005895522cd.

--- TestEnvironment/Assets/_Scripts/LightChanger.cs
+++ TestEnvironment/Assets/_Scripts/LightChanger.cs
@@ -5,30 +5,24 @@ using UnityEngine;
 public class LightChanger : MonoBehaviour
 {
     public float num;
-    public bool itsTime;
-    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         SetBlend(num);
-
-        if (itsTime)
-        {
-            cambiarSkybox();
-        }
     }
     
     public void SetBlend(float num)
     {
         RenderSettings.skybox.SetFloat("_Blend", num);
     }
-    
-    public void cambiarSkybox()
-    {
-        num = num - 0.1f * Time.deltaTime;
-        if (num <= 0)
-        {
-            itsTime = false;
-        }
-    }
+
+
 }
