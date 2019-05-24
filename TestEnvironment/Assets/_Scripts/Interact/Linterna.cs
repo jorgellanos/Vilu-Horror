d7f08c6ee5cac41a38bc8262092286547b7e07f4
jorgellanos <jorg.llanos@hotmail.com>
2019-05-24 18:40:48 -0400

    linterna stuff

---
+++ TestEnvironment/Assets/_Scripts/Interact/Linterna.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    // Variables
+    public bool on;
+
+    // Reference
+    public Hand currentHand;
+    public Light lt;
+    private Transform pointer;
+    private GameObject eventTrigger;
+
+    // RayCast
+    public RaycastHit hit;
+    public Ray landingRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointer = transform;
+        on = true;
+        currentHand = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Raycasting();
+
+        if (currentHand != null)
+        {
+            turnOnOff();
+        }
+
+        if (on)
+        {
+            lt.enabled = true;
+        }
+        else
+        {
+            lt.enabled = false;
+        }
+
+        Debug.DrawRay(pointer.position, transform.forward * 20,Color.red, 0.001f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hand")
+        {
+            currentHand = other.GetComponent<Hand>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hand")
+        {
+            currentHand = null;
+        }
+    }
+
+    public void turnOnOff()
+    {
+        if (currentHand.isUsing)
+        {
+            if (on)
+            {
+                on = false;
+            }
+            else
+            {
+                on = true;
+            }
+        }
+    }
+
+    public void Raycasting()
+    {
+        // Ray direction
+        landingRay = new Ray(pointer.position, pointer.forward);
+        
+        if (Physics.Raycast(landingRay, out hit, 20f))
+        {
+            GetEvent();
+        }
+        else
+        {
+            // NOTHING YET
+        }
+    }
+
+    public void GetEvent()
+    {
+        if (hit.collider.gameObject.name == "Stairs")
+        {
+            Debug.Log("SOOOOOOOOOOUUUUUUUUUNNNNNNNDDDDD");
+        }
+    }
+}
